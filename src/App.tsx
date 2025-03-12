276dcc1ba89226201987af54dad7f82da22faa80
Sergio Muro <sergiomuro@gmail.com>
2025-03-12 17:11:35 -0300

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 
 import MoviesGrid from "./components/MoviesGrid/MoviesGrid";
 import MovieDetails from "./components/MovieDetails/MovieDetails";
 
-import './_App.css';  // Aquí es donde se importa el archivo global de estilos
+import './_App.css';
+
+const APP_NAME = import.meta.env.VITE_APP_NAME || "Movies Tau One";
 
 const App: React.FC = () => {
+
+  useEffect(() => {
+    return () => {
+      document.title = APP_NAME;
+    };
+  }, []);
+
   return (
       <Router>
         <Routes>
